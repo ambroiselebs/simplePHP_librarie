933ec89af1456b766126775844585d4f360bc9e0
ambroiselebs <amberenger06@gmail.com>
2022-06-01 17:40:07 +0200

    bugs

--- createPHPfile.bat
+++ createPHPfile.bat
@@ -1,3 +1,4 @@
+
 @echo off
 if ["%1"] == [""] (
     echo You must run : .\createPHPfile.bat "NameOfYourFile (without ".php")".
@@ -5,7 +6,7 @@ if ["%1"] == [""] (
 ) else (
     (
     echo(^<?php
-    echo(   ^require^("components/requires.php"^);
+    echo(   ^require^("../components/requires.php"^);
     echo(   session_start^(^);
     echo(^?^>
     echo(
@@ -13,10 +14,13 @@ if ["%1"] == [""] (
     echo(^<head^>
     echo(   ^<meta charset="UTF-8"^>
     echo(   ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^>
-    echo(   ^<title^>^<?php echo $websiteName; ?^></title^>
+    echo(   ^<link rel="stylesheet" href="css/reset.css"^>
+    echo(   ^<link rel="stylesheet" href="css/style.css"^>
+    echo(   ^<title^>^<?php echo $websiteName; ?^>^</title^>
     echo(^</head^>
     echo(^<body^>
     echo(
+    echo(   ^<h1^>Hello World!^</h1^>
     echo(^</body^>
     echo(^</html^>
     )>src/%1.php
